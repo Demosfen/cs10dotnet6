67a3a150004466f1a8ca37c35f40d9d707fa4413
Alexander <demosfen.spb@gmail.com>
2022-09-11 14:52:36 +0300

    Small updates of Variables

--- Chapter02-vscode/Variables/Program.cs
+++ Chapter02-vscode/Variables/Program.cs
@@ -1,4 +1,6 @@
-﻿object height = 1.88; // storing a double in an object
+﻿using System.Xml;
+
+object height = 1.88; // storing a double in an object
 object name = "Amir"; // storing a string in an object
 Console.WriteLine($"{name} is {height} metres tall.");
 // int length1 = name.Length; // gives compile error!
@@ -18,3 +20,33 @@ something = new[] { 3, 5, 7 };
 // if you later store a data type that does not have a
 // property named Length
 Console.WriteLine($"Length is {something.Length}");
+
+// ************************************************
+
+int population = 66_000_000; // 66 million in UK
+double weight = 1.88; // in kilograms
+decimal price = 4.99M; // in pounds sterling
+string fruit = "Apples"; // strings use double-quotes
+char letter = 'Z'; // chars use single-quotes
+bool happy = true; // Booleans have value of true or false
+
+// ************************************************
+
+var population = 66_000_000; // 66 million in UK
+var weight = 1.88; // in kilograms
+var price = 4.99M; // in pounds sterling
+var fruit = "Apples"; // strings use double-quotes
+var letter = 'Z'; // chars use single-quotes
+var happy = true; // Booleans have value of true or false
+
+// ************************************************
+
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
