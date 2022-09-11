52d3e67e40652b0320b9ad4b4afef1cdefea37ed
Alexander <demosfen.spb@gmail.com>
2022-09-11 13:19:18 +0300

    Updates for Chapter02

--- Chapter02-vscode/Vocabulary/obj/Debug/net6.0/Vocabulary.GlobalUsings.g.cs
+++ Chapter02-vscode/Vocabulary/obj/Debug/net6.0/Vocabulary.GlobalUsings.g.cs
@@ -4,5 +4,5 @@ global using global::System.Collections.Generic;
 global using global::System.IO;
 global using global::System.Linq;
 global using global::System.Net.Http;
-global using global::System.Threading;
+global using global::System.Numerics;
 global using global::System.Threading.Tasks;
