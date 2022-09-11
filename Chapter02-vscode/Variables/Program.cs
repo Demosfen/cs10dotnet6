d8d81b653ad526443184b6e8d4a376d353c25e22
Alexander <demosfen.spb@gmail.com>
2022-09-11 14:38:43 +0300

    Third Update for Variables

---
+++ Chapter02-vscode/Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿object height = 1.88; // storing a double in an object
+object name = "Amir"; // storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall.");
+// int length1 = name.Length; // gives compile error!
+int length2 = ((string)name).Length; // tell compiler it is a string
+Console.WriteLine($"{name} has {length2} characters.");
+
+// ************************************************
+
+// storing a string in a dynamic object
+// string has a Length property
+dynamic something = "Ahmed";
+// int does not have a Length property
+something = 12;
+// an array of any type has a Length property
+something = new[] { 3, 5, 7 };
+// this compiles but would throw an exception at run-time
+// if you later store a data type that does not have a
+// property named Length
+Console.WriteLine($"Length is {something.Length}");
