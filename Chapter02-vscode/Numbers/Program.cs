8bed6849a42c0643a7628a9172c3ea579dcc27ca
Demosfen <Demosfen.spb@gmail.com>
2022-09-19 17:35:49 +0300

    Excersize 2... Done!

--- Chapter02-vscode/Numbers/Program.cs
+++ Chapter02-vscode/Numbers/Program.cs
@@ -10,22 +10,22 @@
 // // double means double-precision floating point
 // double anotherRealNumber = 2.3; // double literal
 
-// // ************************************************
+// ************************************************
 
-// // three variables that store the number 2 million
-// int decimalNotation = 2_000_000;
-// int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
-// int hexadecimalNotation = 0x_001E_8480;
-// // check the three variables have the same value
-// // both statements output true
-// Console.WriteLine($"{decimalNotation == binaryNotation}");
-// Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+// three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+// check the three variables have the same value
+// both statements output true
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
 
-// // ************************************************
+// ************************************************
 
-// Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
-// Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
-// Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
 
 // // ************************************************
 
@@ -43,14 +43,14 @@
 
 // ************************************************
 
-Console.WriteLine("Using decimals:");
-decimal c = 0.1M; // M suffix means a decimal literal value
-decimal d = 0.2M;
-if (c + d == 0.3M)
-{
-Console.WriteLine($"{c} + {d} equals {0.3M}");
-}
-else
-{
-    Console.WriteLine($"{c} + {d} does NOT equal {0.3M}");
-}
+// Console.WriteLine("Using decimals:");
+// decimal c = 0.1M; // M suffix means a decimal literal value
+// decimal d = 0.2M;
+// if (c + d == 0.3M)
+// {
+// Console.WriteLine($"{c} + {d} equals {0.3M}");
+// }
+// else
+// {
+//     Console.WriteLine($"{c} + {d} does NOT equal {0.3M}");
+// }
