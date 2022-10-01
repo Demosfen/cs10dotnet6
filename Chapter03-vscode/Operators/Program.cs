14f2552160278f96bd2b987ed24e3b64b8827747
Alexander <demosfen.spb@gmail.com>
2022-10-01 22:39:35 +0300

    3rd Chapter ex.

---
+++ Chapter03-vscode/Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+int c = ++a;
+WriteLine($"c is {c}");
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+double g = 11;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
+int p = 6;
+p += 3; // equivalent to p = p + 3;
+p -= 3; // equivalent to p = p - 3;
+p *= 3; // equivalent to p = p * 3;
+p /= 3; // equivalent to p = p / 3;
+WriteLine($"p after all operations is {p}");
