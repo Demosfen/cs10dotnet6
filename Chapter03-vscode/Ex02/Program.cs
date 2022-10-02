f46be68312309212c9f273f6e80580bbe03a3f2a
Alexander <demosfen.spb@gmail.com>
2022-10-02 14:16:24 +0300

    Ex02. Chapter03

---
+++ Chapter03-vscode/Ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+try
+{
+    checked
+    {
+        int max = 500;
+        for (byte i = 0; i < max; i++)
+        {
+            WriteLine(i);
+        }
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("I've got OverFlow exception!");
+}
