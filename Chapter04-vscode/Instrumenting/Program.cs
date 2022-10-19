288ff207e5c8a0fd5990f21a8bb26c642dbad4a9
Demosfen <Demosfen.spb@gmail.com>
2022-10-19 21:57:42 +0300

    InNstrumenting adding

---
+++ Chapter04-vscode/Instrumenting/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using System.Diagnostics;
+
+// write to a text file in the project folder
+Trace.Listeners.Add(new TextWriterTraceListener(
+File.CreateText(Path.Combine(Environment.GetFolderPath(
+Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+// text writer is buffered, so this option calls
+// Flush() on all listeners after writing
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says: I'm watching!");
+Trace.WriteLine("Trace says: I'm watching!");
