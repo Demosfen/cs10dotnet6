1fd047ead5849a21ba98bdb5ed0f578800f07064
Demosfen <Demosfen.spb@gmail.com>
2022-10-23 14:57:00 +0300

    Finishing 4th Chapter

--- Chapter04-vscode/Instrumenting/Program.cs
+++ Chapter04-vscode/Instrumenting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 // write to a text file in the project folder
 Trace.Listeners.Add(new TextWriterTraceListener(
@@ -11,3 +12,15 @@ Trace.AutoFlush = true;
 
 Debug.WriteLine("Debug says: I'm watching!");
 Trace.WriteLine("Trace says: I'm watching!");
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+IConfigurationRoot configuration = builder.Build();
+TraceSwitch ts = new(
+displayName: "PacktSwitch",
+description: "This switch is set via a JSON config.");
+configuration.GetSection("PacktSwitch").Bind(ts);
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
--- Chapter04-vscode/WritingFunctions/Program.cs
+++ Chapter04-vscode/WritingFunctions/Program.cs
@@ -130,27 +130,30 @@ internal class Program
         }
 
         static int FibImperative(int term)
-{
-    if (term == 1)
-{
-return 0;
-}
-else if (term == 2)
-{
-return 1;
-}
-else
-{
-return FibImperative(term - 1) + FibImperative(term - 2);
-}
-}
-static void RunFibImperative()
-{
-for (int i = 1; i <= 30; i++)
-{
-WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
-arg0: CardinalToOrdinal(i),
-arg1: FibImperative(term: i));
+        {
+            if (term == 1)
+            {
+                return 0;
+            }
+            else if (term == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return FibImperative(term - 1) + FibImperative(term - 2);
+            }
+        }
+        static void RunFibImperative()
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+                arg0: CardinalToOrdinal(i),
+                arg1: FibImperative(term: i));
+            }
+
+        }
+    }
 }
 
-}
