77347eded6050de06c45b25b3e4d636225a85777
Demosfen <Demosfen.spb@gmail.com>
2022-11-20 14:58:10 +0300

    Exerceis: Person, Student, Professor

---
+++ Exercises/ExerciseLibrary/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Console;
+
+namespace Exercise.Library;
+public class Person : Object
+{
+    public readonly string Name = "John";
+    public void SayHello()
+    {
+        return $"Hello! My name is {Name}";
+    }
+    public SetAge(int age)
+    {
+        int age 
+    }
+
+}
