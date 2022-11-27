d2186beac3401045cbfc7928ba05ac407e0b865f
Demosfen <Demosfen.spb@gmail.com>
2022-11-27 16:44:41 +0300

    Exercise OOP1. Overall update

--- OOP01/ConsoleApp/Program.cs
+++ OOP01/ConsoleApp/Program.cs
@@ -6,11 +6,11 @@ sosok.SayHello();
 
 Student alex = new("Alex Ozr");
 alex.SetAge(35);
+alex.SayHello();
 alex.Study();
 alex.SayAge();
 
 Professor masha = new("Masha Voloshina");
 masha.SetAge(65);
-masha.SetAge(20);
 masha.SayHello();
 masha.Explain();
