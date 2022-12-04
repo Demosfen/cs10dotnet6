ddda11647094d6edbe712aa3818895b86e2c7232
Demosfen <Demosfen.spb@gmail.com>
2022-12-04 15:45:10 +0300

    Chapter 6, example fix sln file

--- Chapter06-vscode/PeopleApp/Program.cs
+++ Chapter06-vscode/PeopleApp/Program.cs
@@ -36,3 +36,20 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 harry.Poke();
+
+// non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2; // lookup the value that has 2 as its key
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
