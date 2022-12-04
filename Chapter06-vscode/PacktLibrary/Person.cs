175ac14f944db2546f04c7cd5a69ac91e31c58fa
Demosfen <Demosfen.spb@gmail.com>
2022-12-04 10:21:09 +0300

    Chapter 6 examples

---
+++ Chapter06-vscode/PacktLibrary/Person.cs
@@ -0,0 +1,77 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person: object
+{
+    //fields
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+    public EventHandler? Shout;
+    public int AngerLevel;
+
+        //static method to multiply
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1} and {p2}"
+        };
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+    //Instance method of procreate
+    public Person ProcreateWith (Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);    
+    }
+
+    //Methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+    }
+
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+                $"{nameof(number)} cannot be less than zero.");
+        }
+
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)
+        {
+            if (localNumber < 1) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+
+    public void Poke()
+    {
+        AngerLevel++;
+
+        if (AngerLevel >= 3)
+        {
+
+            Shout?.Invoke(this, EventArgs.Empty);
+ /*           if (Shout != 0)
+            {
+                Shout(this, EventArgs.Empty);
+            }  */
+        }
+    }
+
+}
---
+++ Chapter06-vscode/PeopleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+Person baby2 = Person.Procreate(harry, jill);
+Person baby3 = harry * mary;
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+WriteLine(
+format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[0].Name);
+
+WriteLine($"5! is {Person.Factorial(5)}");
+
+static  void Harry_Shout(object? sender, EventArgs e)
+    {
+        if (sender is null) return;
+        Person p = (Person)sender;
+        WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+    }
+
+harry.Shout = Harry_Shout;
+
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
