63ed7dc80894b632fe7cb67c69deb15bea7e8235
Alexander <demosfen.spb@gmail.com>
2022-12-25 13:25:18 +0300

    LeetCode ClimbingStairs program.

---
+++ LeetCode/ClimbingStairs/ClimbingLib/ClimbingLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Climbing.Lib;
+
+public class Stairs
+{
+    /// <summary>
+    /// Fibonacchi (n-1)th element
+    /// </summary>
+    private int fib1 = 1;
+    /// <summary>
+    /// Fibonacchi nth element
+    /// </summary>
+    private int fib2 = 1;
+    public int ClimbingStairs(int n)
+    {
+        while (n-- > 0)
+        {
+            fib2 += fib1;
+            fib1 = fib2 - fib1;
+        }
+
+        return fib1;
+    }
+
+}
---
+++ LeetCode/ClimbingStairs/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+using Climbing.Lib;
+
+Stairs stairs1 = new();
+int stair = 6;
+
+WriteLine($"Stairs consistiong of {stair} has {stairs1.ClimbingStairs(stair)} of climbing.");
