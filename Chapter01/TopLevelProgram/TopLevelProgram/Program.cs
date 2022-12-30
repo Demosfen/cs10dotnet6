c4aa8acb1cc0df48d508224b20adc8aeaf5202a3
Alexander <demosfen.spb@gmail.com>
2022-12-30 20:38:26 +0300

    Overall repo update

--- Chapter01/TopLevelProgram/TopLevelProgram/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from a Top Level Program!");
-Console.WriteLine(Environment.OSVersion.VersionString);
-
--- Chapter05-vscode/PacktLibrary/BankAccount.cs
+++
@@ -1,9 +0,0 @@
-namespace Packt.Shared
-{
-    public class BankAccount
-    {
-        public string AccountName;
-        public decimal Balance;
-        public static decimal InterestRate;
-    }
-}
