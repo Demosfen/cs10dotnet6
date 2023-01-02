93b34a1d45c364f047e4d5874164f449e7000511
Alexander <demosfen.spb@gmail.com>
2023-01-02 13:49:03 +0300

    New LeetCode simple Exercise start

---
+++ LeetCode/SingleNumber/SingleNumber.Algorythm/Numbers.cs
@@ -0,0 +1,11 @@
+﻿namespace SingleNumber.Common;
+
+public sealed class Numbers
+{
+    /// <summary>
+    /// Array of integers with only one single number
+    /// </summary>
+    private IEnumerable<int> _ints;
+    
+    
+}
