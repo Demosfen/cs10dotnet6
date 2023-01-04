a0440f62d2be39bf2fbdd3b5477c39f70fa76e02
Alexander <demosfen.spb@gmail.com>
2023-01-04 20:42:07 +0300

    Warehouse: small current updates

--- WMS/Data/Box.cs
+++ WMS/Data/Box.cs
@@ -5,4 +5,9 @@ public class Box : StorageUnit
     public Box(string id, decimal width, decimal height, decimal depth, decimal weight) : base(id, width, height, depth, weight)
     {
     }
+
+    public Box() : base(BASE)
+    {
+        throw new NotImplementedException();
+    }
 }
--- WMS/Data/Palette.cs
+++ WMS/Data/Palette.cs
@@ -10,4 +10,9 @@ public class Palette : StorageUnit
     public Palette(string id, decimal width, decimal height, decimal depth, decimal weight) : base(id, width, height, depth, weight)
     {
     }
+
+    public Palette() : base(BASE)
+    {
+        throw new NotImplementedException();
+    }
 }
