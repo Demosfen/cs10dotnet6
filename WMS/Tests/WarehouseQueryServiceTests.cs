90202f3adbeee24dfb6a24e35aab5919e4d3be2a
Alexander <demosfen.spb@gmail.com>
2023-02-02 18:12:42 +0300

    Unit Tests: small updates

--- WMS/Tests/WarehouseQueryServiceTests.cs
+++ WMS/Tests/WarehouseQueryServiceTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using FluentAssertions;
 using WMS.Data;
 using WMS.Services.Concrete;
@@ -10,8 +11,9 @@ public class WarehouseQueryServiceTests
 {
     private readonly WareHouseQueryService _sut = new();
     
-    [Theory, MemberData(nameof(SortTestData))]
-    public void Sort_ByExpiryAndWeight_ShouldReturnSortedPalettes(Warehouse warehouse, IReadOnlyCollection<Palette> expectedData)
+    [Theory,DisplayName("Group all pallets by expiration date, and sort them in ascending order of expiration date. Sort pallets by weight in each group.")]
+    [MemberData(nameof(SortTestData))]
+    public void SortPalettes_ByExpiryAndWeight(Warehouse warehouse, IReadOnlyCollection<Palette> expectedData)
     {
         var expected = expectedData
             .Where(p => p.ExpiryDate.HasValue)
@@ -23,8 +25,9 @@ public class WarehouseQueryServiceTests
         _sut.SortByExpiryAndWeight(warehouse).Should().BeEquivalentTo(expected);
     }
 
-    [Theory, MemberData(nameof(SortTestData))]
-    public void GetThreePalettes_ByExpiryAndVolume_ShouldReturnSortedPalettes(Warehouse warehouse, IReadOnlyCollection<Palette> expectedData)
+    [Theory,DisplayName("Display the top 3 pallets that contain the boxes with the longest shelf life, sorted in ascending order of volume.")] 
+    [MemberData(nameof(SortTestData))]
+    public void GetThreePalettes_SortedByExpiryAndVolume(Warehouse warehouse, IReadOnlyCollection<Palette> expectedData)
     {
         var expected = expectedData
             .OrderByDescending(p => p.ExpiryDate)
