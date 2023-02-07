de9a555455ac2d0e028adb644b96a82fd750c4fb
alexander <demosfen.spb@gmail.com>
2023-02-07 19:28:22 +0300

    Small updates

--- WMS/ConsoleApp/Program.cs
+++ WMS/ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using WMS.Store.Entities;
 using Microsoft.EntityFrameworkCore;
 using WMS.Services.Concrete;
 
+using static System.Console;
+
 namespace WMS.ConsoleApp;
 
 internal class Program
@@ -19,13 +21,18 @@ internal class Program
         var Palette1 = new Palette(1, 1, 1);
 
         var Palette2 = new Palette(2, 2, 2);
+
+        var Palette3 = new Palette(3, 3, 3);
             
         
         warehouseRepository.AddPalette(warehouse, Palette1);
         warehouseRepository.AddPalette(warehouse, Palette2);
+        warehouseRepository.AddPalette(warehouse, Palette3);
         
         paletteRepository.AddBox(Palette1, new Box(0.1m,0.1m,0.1m,10,new DateTime(2010,01,01)));
         paletteRepository.AddBox(Palette2, new Box(0.2m,0.3m,0.4m,12, new DateTime(2011,1,1)));
+        paletteRepository.AddBox(Palette3, new Box(0.3m, 0.3m, 0.3m, 15, new DateTime(2007,06,03)));
+        paletteRepository.AddBox(Palette3, new Box(0.4m, 0.4m, 0.4m,20, new DateTime(2006,01,01)));
         
         await using var context = new WarehouseContext();
         await context.Database.MigrateAsync();
@@ -33,6 +40,15 @@ internal class Program
         context.Warehouses.Add(warehouse);
         await context.SaveChangesAsync();
         Console.WriteLine("Success!");
+
+        object threePalettes = await context.Palettes
+            .Where(p => p.ExpiryDate.HasValue)
+            .OrderBy(p => p.ExpiryDate)
+            .ThenBy(p => p.Weight)
+            .GroupBy(g => g.ExpiryDate)
+            .ToArrayAsync();
+
+        WriteLine(threePalettes);
 
         /*// create warehouse repo, serializing/deserializing warehouse, save and load: ok!
         WarehouseRepository repository = new();
