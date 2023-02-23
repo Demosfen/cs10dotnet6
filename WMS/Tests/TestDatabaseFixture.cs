08784e32895fff68e091a1b79d78d82bc7c515d0
Alexander <demosfen.spb@gmail.com>
2023-02-23 17:41:32 +0300

    Testing wars

---
+++ WMS/Tests/TestDatabaseFixture.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using WMS.Repositories.Concrete;
+
+namespace WMS.Tests;
+
+public class TestDatabaseFixture
+{
+    private const string DbFileName = "TestDatabase";
+    private const string ConnectionString = $"Data Source=../{DbFileName}";
+    
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using (var context = CreateContext())
+                {
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                    
+                    var unitOfWork = new UnitOfWork()
+                }
+
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    public WarehouseDbContext.WarehouseDbContext CreateContext()
+        => new WarehouseDbContext.WarehouseDbContext(
+            new DbContextOptionsBuilder<WarehouseDbContext.WarehouseDbContext>()
+                .UseSqlite(ConnectionString)
+                .Options);
+}
