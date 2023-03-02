a15c4e3462e84e776a6cd0b5a205e018d9911bd2
Alexander <demosfen.spb@gmail.com>
2023-03-02 13:44:04 +0300

    Chapter 12 finished

---
+++ Book/Chapter12/AsyncEnumerable/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+await foreach (int number in GetNumbersAsync())
+{
+    WriteLine($"Number: {number}");
+}
+
+static async IAsyncEnumerable<int> GetNumbersAsync()
+{
+    Random r = new();
+// имитация работы
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+}
