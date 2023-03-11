c3749459f818051f5bdda0398b19545a0f754557
Alexander <demosfen.spb@gmail.com>
2023-03-11 22:06:08 +0300

    BoxTests done

--- WMS/Tests/Entities/BoxTests.cs
+++ WMS/Tests/Entities/BoxTests.cs
@@ -19,8 +19,25 @@ public class BoxTests : WarehouseTestsBase
         : base(fixture)
     {
     }
+    
+    [Fact(DisplayName = "Box Expiry equals Production plus 100 days by default")]
+    public async Task BoxExpiry_ShouldBeEqual_ProductionPlusHundred()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes(WarehouseName, 1, 0);
+        
+        var box = new Box(warehouse.Palettes[0].Id, 
+            1, 1, 1, 10, 
+            productionDate: new DateTime(2008, 1, 1));
 
-    [Fact(DisplayName = "Box size cannot be negative")]
+        // Act
+        var result = new DateTime(2008, 1, 1).AddDays(100);
+
+        // Assert
+        box.ExpiryDate.Should().Be(result);
+    }
+
+    [Fact(DisplayName = "Box size cannot be negative and should throw ArgumentException")]
     public async Task BoxSize_ShouldNotBe_Negative()
     {
         // Arrange
@@ -61,6 +78,20 @@ public class BoxTests : WarehouseTestsBase
             1, 1, 1, 10, 
             new DateTime(2008, 1, 1),
             new DateTime(2007, 1,1));
+
+        // Assert
+        box.Should().Throw<ArgumentException>();
+    }
+    
+    [Fact(DisplayName = "Box ExpiryDate and ProductionDate should not be equal zero simultaneously")]
+    public async Task BoxExpiryAndProduction_ShouldNotBeEmpty()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes(WarehouseName, 1, 0);
+
+        // Act
+        Action box = () => new Box(warehouse.Palettes[0].Id,
+            1, 1, 1, 10);
 
         // Assert
         box.Should().Throw<ArgumentException>();
