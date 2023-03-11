1a68f07db891dd2f0aa6098f71aec6055012ee76
Alexander <demosfen.spb@gmail.com>
2023-03-11 15:26:18 +0300

    Repositories tests

---
+++ WMS/Tests/Repositories/PaletteRepositoryTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using WMS.Repositories.Concrete;
+using WMS.Store.Entities;
+using WMS.Tests.Abstract;
+using WMS.Tests.Infrastructure;
+
+namespace WMS.Tests.Repositories;
+
+public class PaletteRepositoryTests : WarehouseTestsBase
+{
+    private readonly PaletteRepository _sut;
+
+    public PaletteRepositoryTests(TestDatabaseFixture fixture)
+        : base(fixture)
+    {
+        _sut = new PaletteRepository(DbContext);
+    }
+    
+    [Fact(DisplayName = "Check if palette repository successfully adds and saves palettes to the existing warehouse")]
+    public async Task RepositoryAdd_ShouldAddPaletteToTheWarehouse()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes("Warehouse#1", 3, 1);
+        
+        // Act
+        await _sut.AddAsync(new Palette(warehouse.Id, 10, 10, 10), default);
+        await _sut.UnitOfWork.SaveChangesAsync();
+
+        // Assert
+        var result = await DbContext.Warehouses
+            .FirstOrDefaultAsync(x => x.Name == "Warehouse#1");
+        
+        result?.Palettes.Count.Should().Be(4);
+    }
+    
+    [Fact(DisplayName = "Check if palette repository softly removes palettes from the existing warehouse")]
+    public async Task RepositoryDelete_ShouldSoftlyDeletePalette()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes("Warehouse#2", 3, 1);
+        
+        // Act
+        await _sut.DeleteAsync(warehouse.Palettes[0].Id, default);
+        await _sut.UnitOfWork.SaveChangesAsync();
+        
+        // Assert
+        var warehouseTest = await DbContext.Warehouses
+                                     .FirstOrDefaultAsync(x => x.Name == "Warehouse#2");
+
+        var result = warehouseTest?.Palettes[0].IsDeleted; 
+        
+        result.Should().Be(true);
+    }
+    
+    [Fact(DisplayName = "Check if IsDeleted palettes is filtered")]
+    public async Task SoftlyDeletedPalettes_ShouldNotBeInQueryResults()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes("Warehouse#2", 3, 1);
+        
+        // Act
+        await _sut.DeleteAsync(warehouse.Palettes[0].Id, default);
+        await _sut.UnitOfWork.SaveChangesAsync();
+        
+        // Assert
+        var warehouseTest = await DbContext.Warehouses
+            .FirstOrDefaultAsync(x => x.Name == "Warehouse#2");
+
+        var result = warehouseTest?.Palettes.Count; 
+        
+        result.Should().Be(2);
+    }
+    
+    [Fact(DisplayName = "Check if repository successfully adds box to the existing palette")]
+    public async Task AddBoxes_ShouldStoreBoxesToPalette()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes("Warehouse#4", 5, 3);
+
+        // Act
+        await _sut.AddBox(warehouse.Palettes[4].Id,
+            await CreateBoxAsync(warehouse.Palettes[4].Id),
+            default);
+
+        // Assert
+        var result = DbContext.Warehouses
+            .FirstOrDefaultAsync(x => x.Name == "Warehouse#4")
+            .Result?.Palettes[4].Boxes.Count;
+        
+        result.Should().Be(4);
+    }
+
+    [Fact(DisplayName = "Check OversizeException")]
+    public async Task OversizedBox_ShouldThrowOversizeException()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouseWithPalettesAndBoxes("Warehouse#5", 5, 5);
+        
+        
+    }
+}
--- WMS/Tests/Repositories/WarehouseRepositoryTests.cs
+++ WMS/Tests/Repositories/WarehouseRepositoryTests.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
-using WMS.Repositories.Abstract;
 using WMS.Repositories.Concrete;
 using WMS.Store.Entities;
 using WMS.Tests.Abstract;
@@ -10,43 +9,58 @@ namespace WMS.Tests.Repositories;
 
 public class WarehouseRepositoryTests: WarehouseTestsBase
 {
-    private readonly IGenericRepository<Warehouse> _sut;
+    private readonly WarehouseRepository _sut;
 
     public WarehouseRepositoryTests(TestDatabaseFixture fixture) 
         : base(fixture)
     {
-        _sut = new GenericRepository<Warehouse>(DbContext);
+        _sut = new WarehouseRepository(DbContext);
     }
 
     [Fact(DisplayName = "Check if repository successfully inserts and saves entities")]
     public async Task RepositoryInsert_ShouldInsertWarehouse()
     {
         // Arrange, Act
-        await _sut.AddAsync(new Warehouse("Warehouse1"));
+        await _sut.AddAsync(new Warehouse("Warehouse#1"), default);
         await _sut.UnitOfWork.SaveChangesAsync();
 
         // Assert
         var result = await DbContext.Warehouses
-            .FirstOrDefaultAsync(x => x.Name == "Warehouse1");
+            .FirstOrDefaultAsync(x => x.Name == "Warehouse#1");
+        
         result.Should().NotBeNull();
     }
     
-    [Fact(DisplayName = "Check if repository successfully inserts and saves entities")]
+    [Fact(DisplayName = "Check if repository successfully softly deletes entities")]
     public async Task RepositoryDelete_ShouldSoftDeleteWarehouse()
     {
         // Arrange
         var warehouse = new Warehouse("Warehouse2");
         
         // Act
-        await _sut.AddAsync(warehouse);
-        await _sut.UnitOfWork.SaveChangesAsync();
+        await DbContext.AddAsync(warehouse);
+        await DbContext.SaveChangesAsync();
 
-        await _sut.DeleteAsync(warehouse.Id);
+        await _sut.DeleteAsync(warehouse.Id, default);
         await _sut.UnitOfWork.SaveChangesAsync();
 
         // Assert
         var result = await DbContext.Warehouses
             .FindAsync(warehouse.Id);
         result?.IsDeleted.Should().Be(true);
+    }
+    
+    [Fact(DisplayName = "Check if repository successfully adds palette to the warehouse")]
+    public async Task AddPalettes_ShouldStorePalettesToWarehouse()
+    {
+        // Arrange
+        var warehouse = await CreateWarehouse("Warehouse#3");
+        
+        // Act
+        await _sut.AddPalette(warehouse.Id, new Palette(warehouse.Id, 10, 10, 10), default);
+
+        // Assert
+        var result = warehouse.Palettes.Count;
+        result.Should().Be(1);
     }
 }
