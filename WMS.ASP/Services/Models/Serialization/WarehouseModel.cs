b0f21ab48e69e4458b4e53f3b5355fd39f65b82f
Demosfen <Demosfen.spb@gmail.com>
2023-03-26 10:49:03 +0300

    Starting WebApi for WMS: ver. 1.0

--- WMS.ASP/Services/Models/Serialization/WarehouseModel.cs
+++ WMS.ASP/Services/Models/Serialization/WarehouseModel.cs
@@ -1,12 +1,20 @@
+using WMS.ASP.Store.Interfaces;
+
 namespace WMS.ASP.Services.Models.Serialization;
 
 public sealed class WarehouseModel
 {
-    public WarehouseModel(List<PaletteModel> palettes)
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+
+    public List<PaletteModel> Palettes { get; set; } = new();
+
+    public WarehouseModel(List<PaletteModel> palettes, string name)
     {
+        this.Palettes = palettes;
+        Name = name;
         Palettes = palettes;
     }
-
-    public List<PaletteModel> Palettes { get; }
 }
 
