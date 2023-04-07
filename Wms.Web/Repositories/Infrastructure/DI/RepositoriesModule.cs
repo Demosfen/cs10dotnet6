159960a6d80a0d0420179d0122aa3ebb2d53df93
Alexander <demosfen.spb@gmail.com>
2023-04-07 09:24:10 +0300

    Next bug: None of the constructors found on type 'Wms.Web.Repositories.Concrete.WarehouseRepository

--- Wms.Web/Repositories/Infrastructure/DI/RepositoriesModule.cs
+++ Wms.Web/Repositories/Infrastructure/DI/RepositoriesModule.cs
@@ -1,5 +1,6 @@
 using Autofac;
 using Wms.Web.Repositories.Abstract;
+using Wms.Web.Repositories.Concrete;
 
 namespace Wms.Web.Repositories.Infrastructure.DI;
 
@@ -9,6 +10,7 @@ public sealed class RepositoriesModule : Module
     {
         builder
             .RegisterAssemblyTypes(ThisAssembly)
+            // .IfNotRegistered(typeof(WarehouseRepository)).As<IWarehouseRepository>()
             .AssignableTo<IRepository>()
             .AsImplementedInterfaces()
             .InstancePerDependency();
