4e322acaa97518701d628eb18c2c2b42d972ff12
Ermilov Aleksei <alex.ozr@gmail.com>
2023-04-16 10:10:47 +0200

    GenericRepository registration

--- Wms.Web/Repositories/Infrastructure/DI/RepositoriesModule.cs
+++ Wms.Web/Repositories/Infrastructure/DI/RepositoriesModule.cs
@@ -1,7 +1,6 @@
 using Autofac;
 using Wms.Web.Repositories.Abstract;
 using Wms.Web.Repositories.Concrete;
-using Wms.Web.Store.Entities;
 
 namespace Wms.Web.Repositories.Infrastructure.DI;
 
@@ -9,10 +8,8 @@ public sealed class RepositoriesModule : Module
 {
     protected override void Load(ContainerBuilder builder)
     {
-        builder
-            .RegisterAssemblyTypes(ThisAssembly)
-            .AssignableTo<IRepository>()
-            .AsImplementedInterfaces()
-            .InstancePerDependency();
+        builder.RegisterGeneric(typeof(GenericRepository<>))
+            .As(typeof(IGenericRepository<>))
+            .InstancePerLifetimeScope();
     }
 }
