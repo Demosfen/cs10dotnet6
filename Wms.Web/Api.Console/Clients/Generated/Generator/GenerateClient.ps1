42dee7b5ee62b981bdfe61150b22556e697a01da
Demosfen <Demosfen.spb@gmail.com>
2023-05-03 09:38:08 +0300

    Generator code in progress

---
+++ Wms.Web/Api.Console/Clients/Generated/Generator/GenerateClient.ps1
@@ -0,0 +1,15 @@
+﻿$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$GeneratedClientDir = Resolve-Path "$ScriptDir/../Implementations"
+$GeneratedClientFilePath = "$GeneratedClientDir/GeneratedClient.cs"
+$Namespace = "ANikolaev.Wms.Api.Client.Implementations"
+
+if (Test-Path $GeneratedClientFilePath) {
+  Remove-Item $GeneratedClientFilePath
+}
+
+nswag openapi2csclient /parameterDateTimeFormat:"O" /input:$ScriptDir/swagger.json `
+    /namespace:$Namespace /output:$GeneratedClientFilePath `
+    /generateClientInterfaces:true /generateClientClasses:true  `
+    /OperationGenerationMode:MultipleClientsFromFirstTagAndOperationId `
+    /ClientClassAccessModifier:internal `
+    /GenerateNullableReferenceTypes:true
