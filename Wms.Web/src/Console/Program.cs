6f58465056aae6ef055fb2ab26dce76a26a5ee15
alexander <demosfen.spb@gmail.com>
2023-06-06 09:09:14 +0300

    Client refactor and bug fixes

--- Wms.Web/src/Console/Program.cs
+++ Wms.Web/src/Console/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Wms.Web.Client;
 using Wms.Web.Client.Custom.Abstract;
 using Wms.Web.Client.Extensions;
-using Wms.Web.Contracts.Requests;
+using Wms.Web.Console.Extensions;
 
 var serviceCollection = new ServiceCollection();
 
@@ -19,118 +19,14 @@ var serviceProvider = serviceCollection.BuildServiceProvider();
 var client = serviceProvider.GetRequiredService<IWmsClient>();
 
 var warehouseId = Guid.NewGuid();
-
-var requestCreate = new WarehouseRequest
-{
-    Name = warehouseId.ToString()
-};
-
-var resultCreate = await client.WarehouseClient.CreateAsync(warehouseId, requestCreate, CancellationToken.None);
-
-var requestUpdate = new WarehouseRequest
-{
-    Name = "UPD-" + warehouseId
-};
-
-var resultUpdate = await client.WarehouseClient.PutAsync(
-    warehouseId, requestUpdate, CancellationToken.None);
-
-var notDeletedWarehouse = await client.WarehouseClient.GetAllAsync(
-    0, 3, CancellationToken.None);
-
-var paletteCreateRequest = new PaletteRequest
-{
-    Width = 10,
-    Height = 10,
-    Depth = 10
-};
-
 var paletteId = Guid.NewGuid();
-
-var paletteCreate = await client
-    .PaletteClient
-    .CreateAsync(
-        warehouseId,
-        paletteId,
-        paletteCreateRequest,
-        CancellationToken.None);
-
-var palettesNotDeleted = await client
-    .PaletteClient.GetAllAsync(
-        warehouseId,
-        0,1,
-        CancellationToken.None);
-
-var paletteUpdateRequest = new PaletteRequest
-{
-    Width = 20,
-    Height = 20,
-    Depth = 20
-};
-
-var updateResult = await client.PaletteClient
-    .PutAsync(
-    warehouseId,
-    paletteId,
-    paletteUpdateRequest,
-    CancellationToken.None);
-
 var boxId = Guid.NewGuid();
 
-var boxCreateRequest = new BoxRequest
-{
-    Width = 20,
-    Height = 20,
-    Depth = 20,
-    Weight = 10,
-    ExpiryDate = new DateTime(2013,01,01)
-};
+await client.TryWarehouseClient(warehouseId);
 
-var boxCreate = await client.BoxClient
-    .CreateAsync(
-        paletteId,
-        boxId,
-        boxCreateRequest,
-        CancellationToken.None);
+await client.TryPaletteClient(warehouseId, paletteId);
 
-var boxGet = await client.BoxClient.GetAllAsync(
-    paletteId, 0, 5, cancellationToken: CancellationToken.None);
-
-var boxUpdateRequest = new BoxRequest
-{
-    Width = 1,
-    Height = 1,
-    Depth = 1,
-    Weight = 1,
-    ExpiryDate = new DateTime(2010,1,1),
-    ProductionDate = new DateTime(2008,1,1)
-};
-
-var boxUpdate = await client.BoxClient.PutAsync(
-    boxId,
-    paletteId,
-    boxUpdateRequest,
-    CancellationToken.None);
-
-var boxDelete = await client.BoxClient.DeleteAsync(
-    boxId,
-    CancellationToken.None);
-
-var boxGetDeleted = await client.BoxClient.GetAllDeletedAsync(
-    paletteId, 0, 5, cancellationToken: CancellationToken.None);
-
-var deletePalette = await client.PaletteClient.DeleteAsync(paletteId, CancellationToken.None);
-
-var palettesDeleted = await client
-    .PaletteClient.GetAllAsync(
-        warehouseId,
-        0,2,
-        CancellationToken.None);
-
-var warehouseDelete = await client.WarehouseClient.DeleteAsync(warehouseId, CancellationToken.None);
-
-var deletedWarehouse = await client.WarehouseClient.GetAllDeletedAsync(
-    0, 2, CancellationToken.None);
+await client.TryBoxClient(warehouseId, paletteId, boxId);
 
 Console.ReadKey();
 
